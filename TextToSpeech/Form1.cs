3b93593f689b26ea784dbb57cce2609da482deb0
Bamiji Oyetubo <60369677+bigboybamo@users.noreply.github.com>
2021-09-06 14:49:34 +0100

    changemade changes

--- TextToSpeech/Form1.cs
+++ TextToSpeech/Form1.cs
@@ -91,7 +91,7 @@ namespace TextToSpeech
 
             }
         }
-
+        //to resume
         private void BtnResume_Click(object sender, EventArgs e)
         {
             if (isStopped == false)
@@ -198,7 +198,7 @@ namespace TextToSpeech
                 return;
 
             }
-            ///helllo
+            ///hellloo changes
 
             if (txtSpechText.SelectionLength > 0)
             {
